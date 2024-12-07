447d858568d8a5e73f3986bbd5a4dede5c503034
Olof Johansson <olofj@tenstorrent.com>
2024-12-06 18:03:09 -0800

    day06: More cleanups

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,40 +1,21 @@
 use std::collections::HashSet;
 
-const MOVES: &'static [(char, isize, isize)] =
-    &[('^', -1, 0), ('>', 0, 1), ('v', 1, 0), ('<', 0, -1)];
+const MOVES: &'static [(isize, isize)] = &[(-1, 0), (0, 1), (1, 0), (0, -1)];
 
-fn day06a(infile: &str) -> usize {
-    let map = infile
-        .lines()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    let mut pos = (0, 0);
-    let mut dir = '.';
+fn visited(map: &Vec<Vec<char>>, pos: (usize, usize), moveidx: usize) -> HashSet<(usize, usize)> {
+    let mut pos = pos;
+    let mut moveidx = moveidx;
+
     let nlines = map.len();
     let nchars = map[0].len();
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
 
-    for (i, l) in map.iter().enumerate() {
-        for (j, c) in l.iter().enumerate() {
-            if *c != '#' && *c != '.' {
-                pos = (i, j);
-                dir = *c;
-            }
-        }
-    }
-
-    let mut moveidx = MOVES
-        .iter()
-        .enumerate()
-        .filter_map(|(i, m)| if m.0 == dir { Some(i) } else { None })
-        .next()
-        .unwrap();
+    let mut ret: HashSet<(usize, usize)> = HashSet::new();
 
     loop {
-        visited.insert(pos);
+        ret.insert(pos);
         let newpos = (
-            pos.0.checked_add_signed(MOVES[moveidx].1).unwrap_or(nlines),
-            pos.1.checked_add_signed(MOVES[moveidx].2).unwrap_or(nchars),
+            pos.0.checked_add_signed(MOVES[moveidx].0).unwrap_or(nlines),
+            pos.1.checked_add_signed(MOVES[moveidx].1).unwrap_or(nchars),
         );
         if newpos.0 == nlines || newpos.1 == nchars {
             break;
@@ -45,8 +26,27 @@ fn day06a(infile: &str) -> usize {
         }
         pos = newpos;
     }
+    ret
+}
 
-    let ret = visited.len();
+fn day06a(infile: &str) -> usize {
+    let map = infile
+        .lines()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let moveidx = 0;
+
+    let pos = map
+        .iter()
+        .enumerate()
+        .flat_map(|(i, l)| l.iter().enumerate().map(move |(j, c)| (i, j, c)))
+        .find(|(_, _, &c)| c != '#' && c != '.')
+        .map(|(i,j,_)| (i,j))
+        .unwrap();
+
+    let path = visited(&map, pos, moveidx);
+
+    let ret = path.len();
     println!("{}", ret);
     ret
 }
@@ -59,15 +59,15 @@ fn day06a(infile: &str) -> usize {
 fn loops(map: Vec<Vec<char>>, pos: (usize, usize), moveidx: usize) -> bool {
     let mut pos = pos;
     let mut moveidx = moveidx;
-    let mut turned: HashSet<(usize, usize, usize)> = HashSet::new();
+    let mut turns: HashSet<(usize, usize, usize)> = HashSet::new();
 
     let nlines = map.len();
     let nchars = map[0].len();
 
     loop {
         let newpos = (
-            pos.0.checked_add_signed(MOVES[moveidx].1).unwrap_or(nlines),
-            pos.1.checked_add_signed(MOVES[moveidx].2).unwrap_or(nchars),
+            pos.0.checked_add_signed(MOVES[moveidx].0).unwrap_or(nlines),
+            pos.1.checked_add_signed(MOVES[moveidx].1).unwrap_or(nchars),
         );
         if newpos.0 == nlines || newpos.1 == nchars {
             // Walking off the map, we're done (no loop)
@@ -76,11 +76,11 @@ fn loops(map: Vec<Vec<char>>, pos: (usize, usize), moveidx: usize) -> bool {
         if map[newpos.0][newpos.1] == '#' {
             // Hit a wall, update direction and restart
             moveidx = (moveidx + 1) % MOVES.len();
-            if turned.contains(&(pos.0, pos.1, moveidx)) {
+            if turns.contains(&(pos.0, pos.1, moveidx)) {
                 // We have looped if we're about to turn on a tile and direction seen before
                 return true;
             }
-            turned.insert((pos.0, pos.1, moveidx));
+            turns.insert((pos.0, pos.1, moveidx));
             continue;
         }
         pos = newpos;
@@ -94,58 +94,28 @@ fn day06b(infile: &str) -> usize {
         .lines()
         .map(|l| l.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
-    let mut pos = (0, 0);
-    let mut dir = '.';
-    let nlines = map.len();
-    let nchars = map[0].len();
 
-    for (i, l) in map.iter().enumerate() {
-        for (j, c) in l.iter().enumerate() {
-            if *c != '#' && *c != '.' {
-                pos = (i, j);
-                dir = *c;
-            }
-        }
-    }
-
-    let mut moveidx = MOVES
+    let pos = map
         .iter()
         .enumerate()
-        .filter_map(|(i, m)| if m.0 == dir { Some(i) } else { None })
-        .next()
+        .flat_map(|(i, l)| l.iter().enumerate().map(move |(j, c)| (i, j, c)))
+        .find(|(_, _, &c)| c != '#' && c != '.')
+        .map(|(i,j,_)| (i,j))
         .unwrap();
 
-    let orig_pos = pos;
-    let orig_moveidx = moveidx;
+    let moveidx = 0;
 
     // We need to know the default visited tiles, since it's the
     // candidates for new obstructions.
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
-
-    loop {
-        visited.insert(pos);
-        let newpos = (
-            pos.0.checked_add_signed(MOVES[moveidx].1).unwrap_or(nlines),
-            pos.1.checked_add_signed(MOVES[moveidx].2).unwrap_or(nchars),
-        );
-        if newpos.0 == nlines || newpos.1 == nchars {
-            break;
-        }
-        if map[newpos.0][newpos.1] == '#' {
-            moveidx = (moveidx + 1) % MOVES.len();
-            continue;
-        }
-        pos = newpos;
-    }
+    let path = visited(&map, pos, moveidx);
 
     // Then iterate through each visited location and consider if
     // there's a loop created if an obstruction is added there.
-
     let mut ret = 0;
-    for v in visited {
+    for v in path {
         let mut map = map.clone();
         map[v.0][v.1] = '#';
-        if loops(map, orig_pos, orig_moveidx) {
+        if loops(map, pos, moveidx) {
             ret += 1;
         }
     }
