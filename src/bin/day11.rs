04234ce11370b1f2ed3ef61e6eb089cf37d23a78
Olof Johansson <olofj@tenstorrent.com>
2024-12-11 12:28:26 -0800

    day11: cleanups

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,45 +1,41 @@
 use std::collections::HashMap;
 
+fn expand(stones: Vec<usize>) -> Vec<usize> {
+    let mut ret: Vec<usize> = Vec::new();
+
+    for s in stones.into_iter() {
+        if s == 0 {
+            ret.push(1);
+            continue;
+        }
+
+        let digits = s.ilog10() + 1;
+        let div = 10usize.pow(digits / 2);
+        if digits % 2 == 0 {
+            ret.push(s / div);
+            ret.push(s % div);
+        } else {
+            ret.push(s * 2024);
+        }
+    }
+    ret
+}
+
 fn day11a(infile: &str) -> usize {
     let mut stones = infile
         .split_whitespace()
         .map(|v| v.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
+
     for _ in 0..25 {
         stones = stones
             .iter()
             .flat_map(|v| {
-                if *v == 0 {
-                    vec![1]
-                } else if format!("{}", v).len() % 2 == 0 {
-                    let str = format!("{}", v);
-                    let (s1, s2) = str.split_at(str.len() / 2);
-                    vec![s1.parse::<usize>().unwrap(), s2.parse::<usize>().unwrap()]
-                } else {
-                    vec![v * 2024]
-                }
+                expand(vec![*v])
             })
             .collect();
     }
     stones.len()
-}
-
-fn expand(stones: Vec<usize>) -> Vec<usize> {
-    let mut ret: Vec<usize> = Vec::new();
-
-    for i in 0..stones.len() {
-        if stones[i] == 0 {
-            ret.push(1);
-        } else if format!("{}", stones[i]).len() % 2 == 0 {
-            let str = format!("{}", stones[i]);
-            let (s1, s2) = str.split_at(str.len() / 2);
-            ret.push(s1.parse::<usize>().unwrap());
-            ret.push(s2.parse::<usize>().unwrap());
-        } else {
-            ret.push(stones[i] * 2024);
-        }
-    }
-    ret
 }
 
 fn day11b(infile: &str) -> usize {
@@ -52,15 +48,16 @@ fn day11b(infile: &str) -> usize {
     for s in stones.into_iter() {
         h.entry(s).and_modify(|s| *s += 1).or_insert(1);
     }
+
     for _blink in 0..75 {
         for (k, v) in h.clone().into_iter() {
-            // remove count for this entry
             h.entry(k).and_modify(|s| *s -= v);
             for s in expand(vec![k]) {
                 h.entry(s).and_modify(|s| *s += v).or_insert(v);
             }
         }
     }
+
     let ret = h.iter().map(|(_, v)| v).sum::<usize>();
     ret
 }
